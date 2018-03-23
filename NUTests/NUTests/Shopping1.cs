f8091bbfbbd8fd631ce8fd971757916cef4a2da4
eisoftserv <ellailona@gmail.com>
2018-03-23 18:39:17 +0200

    current

---
+++ NUTests/NUTests/Shopping1.cs
@@ -0,0 +1,158 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUTests
+{
+    [TestFixture]
+    [SingleThreaded]
+    [Description("exercises for automationpractice.com")]
+    class Shopping1
+    {
+        internal FirefoxDriver driver = null;
+        internal IJavaScriptExecutor jse = null;
+        
+
+
+        [OneTimeSetUp]
+        [Description("logging in")]
+        public void FF_Login()
+        {
+            string loginUrl = "http://automationpractice.com/";
+            string user = "test@gmail.com";
+            string password = "test";
+
+            // initializing driver and JavaScript executor
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Size = new System.Drawing.Size(1024, 768);
+            jse = (IJavaScriptExecutor)driver;
+            
+            // navigating to the landing page
+            driver.Url = loginUrl;
+
+            // "sign in" workflow
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            var obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("login")));
+            obj.Click();
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("email")));
+            obj.Click();
+            obj.SendKeys(user + Keys.Tab);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.TextToBePresentInElementValue(By.Id("email"), user));
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("passwd")));
+            obj.Click();
+            obj.SendKeys(password + Keys.Tab);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.TextToBePresentInElementValue(By.Id("passwd"), password));
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("SubmitLogin")));
+            obj.Click();
+
+            // normally the "sign out" button should be present now
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("logout")));
+
+
+        } // LoggingIn
+
+
+
+        [OneTimeTearDown]
+        [Description("logging out")]
+        public void FF_Logout()
+        {
+            string logout = "http://automationpractice.com/index.php?mylogout=";
+            driver.Navigate().GoToUrl(logout);
+
+            // normally the "sign in" button should be present now
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var obj = wait.Until(ExpectedConditions.ElementExists(By.ClassName("login")));
+
+            driver.Quit();
+
+        } // LoggingOut
+
+
+
+        [Test]
+        [Order(1)]
+        public void FF_ShopOneItem()
+        {
+            // set focus to the "Women" menu item
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Women")));
+            jse.ExecuteScript("arguments[0].focus()", obj);
+
+            // hit the "Tops" category
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Tops")));
+            obj.Click();
+
+            // hit the "blue" square (I want a blue t-shirt)
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementExists(By.Id("color_2")));
+            jse.ExecuteScript("arguments[0].focus()", obj);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("color_2")));
+            obj.Click();
+
+            // hit the "Add to Cart" button
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("Submit")));
+            obj.Click();
+
+            // close the light-box by hitting "Continue shopping"
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath(".//*[@title='Continue shopping']")));
+            obj.Click();
+
+            // check number of items in Cart
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//*[starts-with(@class,'ajax_cart_quantity')]")));
+            Assert.That(obj.Text.ToString(), Is.EqualTo("1"));
+
+            // displaying Ajax Cart
+            var todo = new Actions(driver);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//a[starts-with(@title,'View my shopping cart')]")));
+            todo.MoveToElement(obj).Perform();
+
+            // removing the item from the Cart
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            obj = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//a[starts-with(@class,'ajax_cart_block_remove_link')]")));
+            obj.Click();
+
+            // normally the SPAN with "(empty)" text should become visible
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            obj = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath(".//*[@class='ajax_cart_no_product']")));
+
+        } // ShopOneItem
+
+
+
+        [Test]
+        [Order(2)]
+        public void FF_ShopTwoItems()
+        {
+
+        } // ShopTwoItems
+
+
+
+    } // class
+
+
+
+} // namespace
