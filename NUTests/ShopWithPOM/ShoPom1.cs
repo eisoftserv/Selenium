ef169e84801e4d0a5cf4a05b67501c604d8056a8
eisoftserv <ellailona@gmail.com>
2018-04-07 16:12:32 +0300

    commit

--- NUTests/ShopWithPOM/ShoPom1.cs
+++ NUTests/ShopWithPOM/ShoPom1.cs
@@ -6,10 +6,18 @@ using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.PageObjects;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace ShopWithPOM
 {
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    } // Product
+
+
     public class LoginPart
     {
         IWebDriver driver;
@@ -62,14 +70,103 @@ namespace ShopWithPOM
         } // Login
 
     } // LoginPart
-
-
-
-    public class DesktopMenuPart
-    {
-
-    } // DesktopMenuPart
     
+
+    public class MenuPart
+    {
+        IWebDriver driver;
+        IWebElement e_dresses => driver.FindElement(By.XPath("//ul[starts-with(@class,'sf-menu')]/li[2]"));
+
+        public MenuPart(IWebDriver drv)
+        {
+            this.driver = drv;
+        } //
+
+
+        public bool BringDresses()
+        {
+            // go to the "Dresses" menu
+            var todo = new Actions(driver);
+            todo.MoveToElement(e_dresses).Click().Perform();
+            // waiting until products are loaded (via Ajax)
+            Helper.FixedWait(10000);
+            return (Helper.FluidWait(10000, driver));
+
+        } // BringDresses
+
+    } // MenuPart
+
+
+    public class OfferPart
+    { 
+        IWebDriver driver;
+        ReadOnlyCollection<IWebElement> e_products => driver.FindElements(By.XPath("//div[@class='product-container']"));
+        ReadOnlyCollection<IWebElement> e_more_buttons => driver.FindElements(By.XPath("//a[contains(@class,'lnk_view')]"));
+
+        public OfferPart(IWebDriver drv)
+        {
+            this.driver = drv;
+        } //
+
+
+        public Product GetSummarytInfo(int position)
+        {
+            Product prod = new Product();
+            // get the name and price of the item
+            var names = driver.FindElements(By.XPath("//div[starts-with(@class,'right-block')]/h5/a"));
+            var prices = driver.FindElements(By.XPath("//div[starts-with(@class,'right-block')]/div/span"));
+            int pos = Math.Min(names.Count-1, prices.Count-1);
+            pos = Math.Min(pos, position);
+            if (pos < 0) return null;
+
+            prod.Name = names[pos].Text;
+            prod.Price = Helper.Money(prices[pos].Text);
+
+            return prod;
+
+        } // GetSummaryInfo
+
+
+        public Product GetMoreViewInfo(int position)
+        {
+            Product prod = new Product();
+            var objs = e_products;
+            var btns = e_more_buttons;
+            int pos = Math.Min(objs.Count - 1, btns.Count - 1);
+            pos = Math.Min(pos, position);
+            if (pos < 0) return null;
+
+            var obj = objs[pos];
+            driver.ExecuteJavaScript("arguments[0].scrollIntoView(true);", obj);
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => {
+                if (obj.Displayed && obj.Enabled) return true;
+                else return false;
+            });
+            var todo = new Actions(driver);
+            todo.MoveToElement(obj).Perform();
+
+            var btn = btns[pos];
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => {
+                if (btn.Displayed && btn.Enabled) return true;
+                else return false;
+            });
+            btn.Click();
+
+            Helper.FluidWait(10000, driver);
+
+            obj = driver.FindElement(By.XPath("//div[starts-with(@class,'pb-center-column')]/h1"));
+            prod.Name = obj.Text;
+            obj = driver.FindElement(By.Id("our_price_display"));
+            prod.Price = Helper.Money(obj.Text);
+
+            return prod;
+
+        } // GetMoreViewInfo
+
+
+    } // OfferPart
 
 
     public class AjaxCartPart
@@ -105,7 +202,7 @@ namespace ShopWithPOM
         {
             string loginUrl = "http://automationpractice.com/index.php?controller=my-account";
             string user = "ellailona2016@gmail.com";
-            string password = "maricosan";
+            string password = "";
 
             // initializing driver
             driver = new FirefoxDriver();
@@ -151,12 +248,21 @@ namespace ShopWithPOM
 
 
         [Test]
-        [Description("test")]
+        [Description("Ticket #6 Product Summary View VS More Detail View")]
         [Order(30)]
-        public void PassTest()
+        public void ProductSummary_MoreView()
         {
-            Assert.Pass("xyxy");
-        } //
+            var menu = new MenuPart(driver);
+            // hitting the menu should return at least one result
+            Assert.That(menu.BringDresses(), Is.True);
+
+            var offer = new OfferPart(driver);
+            Product summary = offer.GetSummarytInfo(0);
+            Product detail = offer.GetMoreViewInfo(0);
+
+            Assert.That(summary!=null && detail!=null && summary.Name==detail.Name && summary.Price==detail.Price, Is.True);
+            
+        } // ProductSummary_MoreView
 
 
 
@@ -167,7 +273,7 @@ namespace ShopWithPOM
     public static class Helper
     {
         // the UI thread is waiting the indicated number of milliseconds
-        public static void MyFixedWait(int fixedMillisec, int fluidMillisec)
+        public static void FixedWait(int fixedMillisec)
         {
             if (fixedMillisec > 0)
             {
@@ -180,7 +286,7 @@ namespace ShopWithPOM
                 tsk.Dispose();
             }
 
-        } // MyFixedWait
+        } // FixedWait
 
 
         // checking if jQuery Ajax is not running and document is completely loaded
@@ -200,7 +306,7 @@ namespace ShopWithPOM
 
 
         // transforms money string to double
-        private static double Money(string what)
+        public static double Money(string what)
         {
             double val;
             Double.TryParse(what.Substring(1), out val);
@@ -209,7 +315,7 @@ namespace ShopWithPOM
 
 
         // transforms string to double
-        private static double Number(string what)
+        public static double Number(string what)
         {
             double val;
             Double.TryParse(what, out val);
